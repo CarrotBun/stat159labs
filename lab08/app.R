7a3e67faf8f332e0d2207ec59088cf604f35f67e
Lily Li <ylilyli24@gmail.com>
2016-10-24 10:03:55 -0700

    filled template with Advertising data

---
+++ lab08/app.R
@@ -0,0 +1,39 @@
+#You will plot scatter plots of sales vs different advertising 
+#channels. The plot displayed will be configured by 
+#the selection of items in a dropdown. 
+
+#To elaborate, Your ShinyDoc will have a dropdown 
+#with a list of 3 options - TV, Radio and newspaper as 
+#an input element. Your output will be a scatter plot 
+#between sales and the input option chosen from the dropdown.
+
+library(shiny)
+data = read.csv("Advertising.csv")
+
+ui <- fluidPage(
+  headerPanel('Choose an advertising budget.'),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('xcol', 'X Variable', choices = c("TV", "Radio", "Newspaper"))),
+    mainPanel(
+      plotOutput('sales_plot')
+    )
+  )
+)
+
+
+server <- function(input, output) {
+  selectedData <- reactive({
+    data[ ,c(input$xcol, "Sales")]
+  })
+  
+  output$sales_plot <- renderPlot({
+    plot(selectedData(),
+         col = blues9,
+         pch = 20, cex = 2)
+  }, height = 500, width = 600)
+  
+}
+
+
+shinyApp(ui = ui, server = server)
